6274758055eea124ca48bcf5f4cc39e2810de3f6
Maouniel <43824101+Maouniel@users.noreply.github.com>
2022-09-24 21:18:11 +0300

    Add files via upload

--- TwitchChatFilter/offline.js
+++ TwitchChatFilter/offline.js
@@ -1,10 +1,11 @@
 ﻿{
-	"version": 1663707752,
+	"version": 1664043350,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-3.4.1.min.js",
 		"offlineClient.js",
+		"images/tiledbackground.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
